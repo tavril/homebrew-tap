34ba62594b8bf7c0da365dedca771f5ca02597f9
Thomas Avril <thomas@avril.swiss>
2024-12-24 15:49:49 +0000

    minfo v0.8.0 -- add logos

--- Formula/minfo.rb
+++ Formula/minfo.rb
@@ -1,13 +1,13 @@
 class Minfo < Formula
   desc "A simple tool to display macOS system information"
   homepage "https://github.com/tavril/minfo"
-  version "0.8.0"
+  version "0.8.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tavril/minfo/releases/download/v0.8.0/minfo-v0.8.0-darwin-arm64.tar.gz"
-      sha256 "39ddb3bcf732ff78561b607524877cf520f795f6f8bf01ba490b39b0e30cc3e9"
+      url "https://github.com/tavril/minfo/releases/download/v0.8.1/minfo-v0.8.1-darwin-arm64.tar.gz"
+      sha256 "b6ae8e7389ebcf8ca5ff3198af4a507f331b4790ed62b11f9160958be173a6a1"
     end
     #on_intel do
     #end
@@ -17,6 +17,9 @@ class Minfo < Formula
     # Extract the binary and move it to the bin directory
     bin.install "minfo"
     man1.install "minfo.1"
+    pkgshare.install "apple"
+    pkgshare.install "apple-nocolor"
+    pkgetc.install "minfo.yml.sample"
   end
 
   test do
