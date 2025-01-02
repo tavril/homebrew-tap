8cb809d58d6f730eebd0d15ac9fc1d024b4b3407
Thomas Avril <thomas@avril.swiss>
2025-01-02 11:33:05 +0100

    minfo v0.10.2

--- Formula/minfo.rb
+++ Formula/minfo.rb
@@ -1,13 +1,13 @@
 class Minfo < Formula
   desc "A simple tool to display macOS system information"
   homepage "https://github.com/tavril/minfo"
-  version "0.10.0"
+  version "0.10.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tavril/minfo/releases/download/v0.10.0/minfo-v0.10.0-darwin-arm64.tar.gz"
-      sha256 "791b00dc04ad7827e794a7caed04e35c3c5e3112661998920010ab6a58e76a9c"
+      url "https://github.com/tavril/minfo/releases/download/v0.10.2/minfo-v0.10.2-darwin-arm64.tar.gz"
+      sha256 "1c76953326578072861a3ad736f965109788771ef3b75cbdd59a882f779cfc49"
     end
     #on_intel do
     #end
